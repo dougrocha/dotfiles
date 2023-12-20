8d27b6b01b87dd9a094eb08e5b784b5caa6f9591
Douglas Rocha <79188161+dougrocha@users.noreply.github.com>
2023-12-20 02:11:40 -0700

    changed harpoon keybinds to work with window movements

--- .config/nvim/lua/plugins/harpoon.lua
+++ .config/nvim/lua/plugins/harpoon.lua
@@ -54,6 +54,7 @@ return {
     vim.keymap.set("n", "<leader>a", function()
       harpoon:list():append()
     end, { desc = "Add current file to harpoon" })
+
     vim.keymap.set("n", "<C-e>", function()
       harpoon.ui:toggle_quick_menu(harpoon:list())
     end, { desc = "Toggle harpoon menu" })
@@ -61,14 +62,13 @@ return {
     vim.keymap.set("n", "<C-t>", function()
       harpoon:list():select(1)
     end, { desc = "Select harpoon item 1" })
-    vim.keymap.set("n", "<C-h>", function()
+
+    vim.keymap.set("n", "<C-n>", function()
       harpoon:list():select(2)
     end, { desc = "Select harpoon item 2" })
-    vim.keymap.set("n", "<C-n>", function()
+
+    vim.keymap.set("n", "<C-b>", function()
       harpoon:list():select(3)
     end, { desc = "Select harpoon item 3" })
-    vim.keymap.set("n", "<C-b>", function()
-      harpoon:list():select(4)
-    end, { desc = "Select harpoon item 4" })
   end,
 }
