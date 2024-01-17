fa5ba90ee9021d8dc12b3a041e8277210646bcb3
Douglas Rocha <79188161+dougrocha@users.noreply.github.com>
2024-01-16 20:18:50 -0700

    changed colorscheme

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,8 +1,46 @@
 return {
-  "Mofiqul/dracula.nvim", -- Dracula Theme
-  lazy = false,
-  priority = 1000,
-  config = function()
-    vim.cmd.colorscheme("dracula")
-  end,
+  {
+    "rebelot/kanagawa.nvim", -- Kanagawa Theme
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require("kanagawa").setup({
+        colors = {
+          theme = {
+            all = {
+              ui = {
+                bg_gutter = "none",
+              },
+            },
+          },
+        },
+        overrides = function(colors)
+          local theme = colors.theme
+          return {
+            TelescopeTitle = { fg = theme.ui.special, bold = true },
+            TelescopePromptNormal = { bg = theme.ui.bg_p1 },
+            TelescopePromptBorder = { fg = theme.ui.bg_p1, bg = theme.ui.bg_p1 },
+            TelescopeResultsNormal = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m1 },
+            TelescopeResultsBorder = { fg = theme.ui.bg_m1, bg = theme.ui.bg_m1 },
+            TelescopePreviewNormal = { bg = theme.ui.bg_dim },
+            TelescopePreviewBorder = { bg = theme.ui.bg_dim, fg = theme.ui.bg_dim },
+            Pmenu = { fg = theme.ui.shade0, bg = theme.ui.bg_p1 }, -- add `blend = vim.o.pumblend` to enable transparency
+            PmenuSel = { fg = "NONE", bg = theme.ui.bg_p2 },
+            PmenuSbar = { bg = theme.ui.bg_m1 },
+            PmenuThumb = { bg = theme.ui.bg_p2 },
+          }
+        end,
+      })
+      require("kanagawa").load("wave")
+    end,
+  },
+  {
+    "Mofiqul/dracula.nvim", -- Dracula Theme
+    enabled = false,
+    lazy = false,
+    priority = 1000,
+    config = function()
+      vim.cmd.colorscheme("dracula")
+    end,
+  },
 }
