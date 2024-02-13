e97a2fe542e006f5d107a578191fe4cd5e7ccb22
Douglas Rocha <79188161+dougrocha@users.noreply.github.com>
2024-02-13 00:46:37 -0700

    added rust inlay hints

--- .config/nvim/lua/plugins/rust.lua
+++ .config/nvim/lua/plugins/rust.lua
@@ -1,8 +1,31 @@
 return {
   {
     "mrcjkb/rustaceanvim",
-    version = "^3",
+    version = "^4",
     ft = { "rust" },
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "mfussenegger/nvim-dap",
+    },
+    config = function()
+      vim.g.rustaceanvim = {
+        inlay_hints = {
+          highlight = "NonText",
+        },
+        tools = {
+          hover_actions = {
+            auto_focus = true,
+          },
+        },
+        server = {
+          on_attach = function(client, bufnr)
+            if client.server_capabilities.inlayHintProvider then
+              vim.lsp.inlay_hint.enable(bufnr, true)
+            end
+          end,
+        },
+      }
+    end,
   },
   {
     "saecki/crates.nvim",
