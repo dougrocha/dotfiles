fff7a5f9b8cc5c93ee6c5ed57d20a0fbe144d4cd
Douglas Rocha <79188161+dougrocha@users.noreply.github.com>
2024-03-09 15:43:51 -0700

    removed format from rust
    since rust formats on it own, no need for auto cmd

--- nvim/lua/plugins/rust.lua
+++ nvim/lua/plugins/rust.lua
@@ -10,23 +10,6 @@ return {
             if client.server_capabilities.inlayHintProvider then
               vim.lsp.inlay_hint.enable(bufnr, true)
             end
-
-            local augroup = vim.api.nvim_create_augroup("LspFormatting", {
-              clear = false,
-            })
-
-            if client.supports_method("textDocument/formatting") then
-              vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-              vim.api.nvim_create_autocmd("BufWritePre", {
-                group = augroup,
-                buffer = bufnr,
-                callback = function()
-                  vim.lsp.buf.format({
-                    bufnr = bufnr,
-                  })
-                end,
-              })
-            end
           end,
         },
       }
