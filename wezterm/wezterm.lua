c087cb38f191326f02b87b876294b28f88e0a497
Douglas Rocha <79188161+dougrocha@users.noreply.github.com>
2024-04-23 12:04:08 -0700

    added rose-pine theme for wezterm

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -6,29 +6,31 @@ if wezterm.config_builder then
 	config = wezterm.config_builder()
 end
 
+config.colors = require("lua/rose-pine").colors()
+config.window_frame = require("lua/rose-pine").window_frame()
+
 -- Theme
 -- config.color_scheme = "Dracula (Official)"
-
 -- Kanagawa Color Scheme
-config.force_reverse_video_cursor = true
-config.colors = {
-	foreground = "#dcd7ba",
-	background = "#1f1f28",
-
-	cursor_bg = "#c8c093",
-	cursor_fg = "#c8c093",
-	cursor_border = "#c8c093",
-
-	selection_fg = "#c8c093",
-	selection_bg = "#2d4f67",
-
-	scrollbar_thumb = "#16161d",
-	split = "#16161d",
-
-	ansi = { "#090618", "#c34043", "#76946a", "#c0a36e", "#7e9cd8", "#957fb8", "#6a9589", "#c8c093" },
-	brights = { "#727169", "#e82424", "#98bb6c", "#e6c384", "#7fb4ca", "#938aa9", "#7aa89f", "#dcd7ba" },
-	indexed = { [16] = "#ffa066", [17] = "#ff5d62" },
-}
+-- config.force_reverse_video_cursor = true
+-- config.colors = {
+-- 	foreground = "#dcd7ba",
+-- 	background = "#1f1f28",
+--
+-- 	cursor_bg = "#c8c093",
+-- 	cursor_fg = "#c8c093",
+-- 	cursor_border = "#c8c093",
+--
+-- 	selection_fg = "#c8c093",
+-- 	selection_bg = "#2d4f67",
+--
+-- 	scrollbar_thumb = "#16161d",
+-- 	split = "#16161d",
+--
+-- 	ansi = { "#090618", "#c34043", "#76946a", "#c0a36e", "#7e9cd8", "#957fb8", "#6a9589", "#c8c093" },
+-- 	brights = { "#727169", "#e82424", "#98bb6c", "#e6c384", "#7fb4ca", "#938aa9", "#7aa89f", "#dcd7ba" },
+-- 	indexed = { [16] = "#ffa066", [17] = "#ff5d62" },
+-- }
 
 config.window_background_opacity = 0.85
 
