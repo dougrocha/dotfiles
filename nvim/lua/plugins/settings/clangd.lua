ef7f17047f032057310e35226cece7c8f3917403
Douglas Rocha <79188161+dougrocha@users.noreply.github.com>
2024-05-15 01:20:19 -0700

    clangd config

--- nvim/lua/plugins/settings/clangd.lua
+++ nvim/lua/plugins/settings/clangd.lua
@@ -1,5 +1,21 @@
 return {
-  on_attach = function(client)
-    client.server_capabilities.signatureHelpProvider = false
-  end,
+  cmd = {
+    "clangd",
+    "--background-index",
+    "--clang-tidy",
+    "--header-insertion=iwyu",
+    "--completion-style=detailed",
+    "--function-arg-placeholders",
+    "--fallback-style=llvm",
+  },
+  root_dir = require("lspconfig.util").root_pattern(
+    "Makefile",
+    ".clangd",
+    ".clang-tidy",
+    ".clang-format",
+    "compile_commands.json",
+    "compile_flags.txt",
+    "configure.ac",
+    ".git"
+  ),
 }
