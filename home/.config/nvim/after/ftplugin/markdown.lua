8fef027dc074b3f389f7edf365e5f9d90f5136aa
Douglas Rocha <79188161+dougrocha@users.noreply.github.com>
2025-03-30 14:43:48 -0700

    do not commit with this on

--- home/.config/nvim/after/ftplugin/markdown.lua
+++ home/.config/nvim/after/ftplugin/markdown.lua
@@ -1,24 +1,24 @@
 vim.opt.colorcolumn = ''
 vim.opt.spell = true
 
-local client = vim.lsp.start({
-  name = 'test_markdown_lsp',
-  -- cmd = { '/Users/douglasrocha/dev/go_markdown_lsp/main' },
-  cmd = { '/Users/douglasrocha/dev/rust_markdown_lsp/target/release/rust_markdown_lsp' },
-  capabilities = require('blink.cmp').get_lsp_capabilities(),
-  root_dir = vim.fs.root(0, '.git'),
-  on_attach = function(_, buf)
-    vim.bo[buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
-
-    vim.keymap.set('n', 'K', vim.lsp.buf.hover, { desc = 'Hover Information' })
-  end,
-})
-
-if not client then
-  vim.notify('hey, you didnt do good client thing')
-else
-  vim.api.nvim_create_autocmd('FileType', {
-    pattern = 'markdown',
-    callback = function() vim.lsp.buf_attach_client(0, client) end,
-  })
-end
+-- local client = vim.lsp.start({
+--   name = 'test_markdown_lsp',
+--   -- cmd = { '/Users/douglasrocha/dev/go_markdown_lsp/main' },
+--   cmd = { '/Users/douglasrocha/dev/rust_markdown_lsp/target/release/rust_markdown_lsp' },
+--   capabilities = require('blink.cmp').get_lsp_capabilities(),
+--   root_dir = vim.fs.root(0, '.git'),
+--   on_attach = function(_, buf)
+--     vim.bo[buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
+--
+--     vim.keymap.set('n', 'K', vim.lsp.buf.hover, { desc = 'Hover Information' })
+--   end,
+-- })
+--
+-- if not client then
+--   vim.notify('hey, you didnt do good client thing')
+-- else
+--   vim.api.nvim_create_autocmd('FileType', {
+--     pattern = 'markdown',
+--     callback = function() vim.lsp.buf_attach_client(0, client) end,
+--   })
+-- end
