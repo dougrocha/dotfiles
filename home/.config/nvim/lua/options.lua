c427497e86114d3b425f42b14b56fbd96d482b0c
Douglas Rocha <79188161+dougrocha@users.noreply.github.com>
2025-07-11 23:38:19 -0700

    feat: more config changes

--- home/.config/nvim/lua/options.lua
+++ home/.config/nvim/lua/options.lua
@@ -6,18 +6,21 @@ vim.o.winborder = 'rounded'
 -- Enable mouse support
 vim.o.mouse = 'a'
 
+vim.o.backup = false
+vim.o.swapfile = false
 vim.o.undofile = true
 vim.o.undodir = vim.fn.stdpath('cache') .. '/undo'
 vim.opt.wildignore:append({ '.DS_Store' })
 
 -- Completion menu
 vim.o.pumheight = 15
-vim.o.completeopt = 'menu,menuone,noselect'
+vim.opt.completeopt = { 'menu', 'menuone', 'noselect' }
 
 -- Update time
 vim.o.timeoutlen = 500
 vim.o.updatetime = 150
 vim.o.ttimeoutlen = 10
+vim.o.autoread = true
 
 -- Use system clipboard
 vim.o.clipboard = 'unnamedplus'
@@ -30,9 +33,10 @@ vim.o.incsearch = true
 vim.o.infercase = true
 
 -- Scroll size
-vim.o.scrolloff = 4
+vim.o.scrolloff = 8
 vim.o.sidescrolloff = 4
 
+-- Visual
 vim.o.showmode = false
 vim.o.termguicolors = true
 vim.o.signcolumn = 'yes'
@@ -41,16 +45,22 @@ vim.o.signcolumn = 'yes'
 vim.wo.number = true
 
 -- Wrap long lines using words
-vim.o.linebreak = true
+vim.o.wrap = false
+vim.o.linebreak = false
 
 vim.o.shiftwidth = 4
 vim.o.tabstop = 4
+vim.o.softtabstop = 4
 vim.o.et = true
 
 -- Enable auto indentation
 vim.o.autoindent = true
 vim.o.expandtab = true
 vim.o.smartindent = true
+
+-- Window
+vim.o.splitright = true
+vim.o.splitbelow = true
 
 -- Spell checking
 vim.o.spelllang = 'en'
@@ -67,6 +77,9 @@ vim.g.markdown_recommended_style = 0
 if vim.fn.has('nvim-0.11') == 1 then
     vim.opt.completeopt:append('fuzzy')
 end
+
+-- Make words with dash be one word
+vim.opt.iskeyword:append('-')
 
 -- Disable certain healthchecks
 vim.g.loaded_python3_provider = 0
