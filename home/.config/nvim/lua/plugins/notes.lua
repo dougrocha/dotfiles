5f053eea64e28b24b9ac2b5fe85b9ecfa51c02d7
Douglas Rocha <79188161+dougrocha@users.noreply.github.com>
2025-10-04 16:36:27 -0700

    updated notes config

--- home/.config/nvim/lua/plugins/notes.lua
+++ home/.config/nvim/lua/plugins/notes.lua
@@ -32,18 +32,9 @@ end
 return {
     {
         'obsidian-nvim/obsidian.nvim',
-        version = '*', -- recommended, use latest release instead of latest commit
+        version = '*',
+        lazy = false,
         ft = 'markdown',
-        -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
-        event = {
-            --   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
-            --   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/*.md"
-            --   -- refer to `:h file-pattern` for more examples
-            'BufReadPre '
-                .. vim.fn.expand '~'
-                .. '/second-brain/*.md',
-            'BufNewFile ' .. vim.fn.expand '~' .. '/second-brain/*.md',
-        },
         enabled = function()
             return not vim.g.minifiles_active
         end,
@@ -53,17 +44,27 @@ return {
             workspaces = {
                 {
                     name = 'second-brain',
-                    path = vim.fn.expand '~' .. '/second-brain',
+                    path = '~/second-brain',
                 },
             },
+            notes_subdir = 'inbox',
+            new_notes_location = 'notes_subdir',
             legacy_commands = false,
-            notes_subdir = 'index',
+            templates = {
+                folder = 'templates',
+                date_format = '%Y-%m-%d',
+            },
             completion = {
                 nvim_cmp = false,
                 blink = true,
+                min_chars = 2,
             },
             picker = {
                 name = 'fzf-lua',
+            },
+            attachments = {
+                img_folder = 'assets/imgs',
+                confirm_img_paste = true,
             },
             ---@param title string|?
             ---@return string
@@ -78,7 +79,7 @@ return {
                         suffix = suffix .. string.char(math.random(65, 90))
                     end
                 end
-                return tostring(os.time()) .. '-' .. suffix
+                return suffix
             end,
 
             ui = {
