bad1d7c9316661022c42f1262c9ed040a865fc85
Douglas Rocha <79188161+dougrocha@users.noreply.github.com>
2026-01-27 01:59:41 -0700

    feat: added some missing PATHS

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -1,10 +1,18 @@
 set -U fish_greeting
 
 set -gx EDITOR nvim
-set -gx VISUAL code
+set -gx GIT_EDITOR nvim
+set -gx VISUAL nvim
+
+set -gx XDG_CACHE_HOME "$HOME/.cache"
+set -gx XDG_CONFIG_HOME "$HOME/.config"
+set -gx XDG_DATA_HOME "$HOME/.local/share"
+set -gx XDG_STATE_HOME "$HOME/.local/state"
 
 abbr --add vim nvim
 abbr --add lg lazygit
+abbr --add cat bat
+abbr --add tm 'tmux new-session -A -s default'
 
 alias cd z
 
