b69de996535360af3402a292baec332faca016e5
Douglas Rocha <79188161+dougrocha@users.noreply.github.com>
2026-06-03 12:56:48 -0700

    feat: add more icons from maria

--- home/.config/nvim/lua/icons.lua
+++ home/.config/nvim/lua/icons.lua
@@ -21,4 +21,33 @@ M.arrows = {
     down = '',
 }
 
+M.symbol_kinds = {
+    Array = '󰅪',
+    Class = '',
+    Color = '󰏘',
+    Constant = '󰏿',
+    Constructor = '',
+    Enum = '',
+    EnumMember = '',
+    Event = '',
+    Field = '󰜢',
+    File = '󰈙',
+    Folder = '󰉋',
+    Function = '󰆧',
+    Interface = '',
+    Keyword = '󰌋',
+    Method = '󰆧',
+    Module = '',
+    Operator = '󰆕',
+    Property = '󰜢',
+    Reference = '󰈇',
+    Snippet = '',
+    Struct = '',
+    Text = '',
+    TypeParameter = '',
+    Unit = '',
+    Value = '',
+    Variable = '󰀫',
+}
+
 return M
