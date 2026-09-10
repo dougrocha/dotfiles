8366b2a94aaa0a36feeed8d7b846ef29d3ced8f9
Douglas Rocha <79188161+dougrocha@users.noreply.github.com>
2026-09-10 13:17:43 -0700

    feat: macos vulkan paths

--- home/.config/fish/conf.d/vulkan.fish
+++ home/.config/fish/conf.d/vulkan.fish
@@ -1,6 +1,5 @@
 # Vulkan SDK Setup (macOS only)
 if test (uname) = "Darwin"
-    # Try latest symlink first, then fall back to specific version
     if test -d "$HOME/VulkanSDK/latest/macOS"
         set -gx VULKAN_SDK $HOME/VulkanSDK/latest/macOS
     else if test -d "$HOME/VulkanSDK/1.4.350.1/macOS"
@@ -13,5 +12,7 @@ if test (uname) = "Darwin"
         set -gx VK_LAYER_PATH "$VULKAN_SDK/share/vulkan/explicit_layer.d:/opt/homebrew/opt/vulkan-validationlayers/share/vulkan/explicit_layer.d"
         set -gx VK_ICD_FILENAMES "$VULKAN_SDK/share/vulkan/icd.d/MoltenVK_icd.json"
         set -gx PKG_CONFIG_PATH "$VULKAN_SDK/lib/pkgconfig" $PKG_CONFIG_PATH
+        set -gx SDL_VULKAN_LIBRARY "$VULKAN_SDK/lib/libvulkan.dylib"
+        set -gx VK_INSTANCE_CREATE_ENUMERATE_PORTABILITY_BIT_KHR 1
     end
 end
